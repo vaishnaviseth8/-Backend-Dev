76d360003ab94b88ae1371b5374a3b821fadd633
manvi shalya <manvishalya840@gmail.com>
2026-01-16 11:29:11 +0530

    Complete Assignment 1: Event Loop and Utilities

---
+++ Assignment1/app.js
@@ -0,0 +1,9 @@
+const stringUtils = require('./exercise2-stringUtils');
+
+
+const text = "Node js is awesome";
+
+console.log("Capitalized:", stringUtils.capitalize(text));
+console.log("Reversed:", stringUtils.reverse(text));
+console.log("Vowel Count:", stringUtils.countVowels(text));
+
