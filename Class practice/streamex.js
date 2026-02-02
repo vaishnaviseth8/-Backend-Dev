cfca8962085ba9b670ff5bc45b6b5087482d3d61
vaishnaviseth8 <167958246+vaishnaviseth8@users.noreply.github.com>
2026-02-02 15:18:28 +0530

    Delete Class practice directory

--- Class practice/streamex.js
+++
@@ -1,7 +0,0 @@
-const fs =require("fs");
-const readStream=fs.createReadStream("./input.txt",{
-    encoding:"utf-8"
-});
-const writeStream=fs.createWriteStream("./output.txt");
-readStream.pipe(writeStream);
-console.log("File copied using stream and pipe");
