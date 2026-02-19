23a4cf17356fd4d4dc5e721b9abe6b52a63cc19e
vaishnaviseth8 <167958246+vaishnaviseth8@users.noreply.github.com>
2026-02-19 21:34:46 +0530

    Delete Assignment1 directory

--- Assignment1/app.js
+++
@@ -1,9 +0,0 @@
-const stringUtils = require('./exercise2-stringUtils');
-
-
-const text = "Node js is awesome";
-
-console.log("Capitalized:", stringUtils.capitalize(text));
-console.log("Reversed:", stringUtils.reverse(text));
-console.log("Vowel Count:", stringUtils.countVowels(text));
-
